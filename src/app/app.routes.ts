747f8b6742ddb6cedf9eddbf87dde25e16fa304c
tahajaiti <taha.jaiti@gmail.com>
2026-02-23 00:54:35 +0000

    feat: add lazy-loaded app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/home/home').then(m => m.Home),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.routes').then(m => m.authRoutes),
+  },
+  {
+    path: 'jobs',
+    loadChildren: () => import('./features/jobs/jobs.routes').then(m => m.jobsRoutes),
+  },
+  {
+    path: 'favorites',
+    canActivate: [authGuard],
+    loadChildren: () => import('./features/favorites/favorites.routes').then(m => m.favoritesRoutes),
+  },
+  {
+    path: 'applications',
+    canActivate: [authGuard],
+    loadChildren: () => import('./features/applications/applications.routes').then(m => m.applicationsRoutes),
+  },
+  {
+    path: 'profile',
+    canActivate: [authGuard],
+    loadChildren: () => import('./features/profile/profile.routes').then(m => m.profileRoutes),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
