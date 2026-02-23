65b9870d58f66bd7bc20ec0a1f7478328837fbda
tahajaiti <taha.jaiti@gmail.com>
2026-02-23 00:47:22 +0000

    feat: add user model interfaces

---
+++ src/app/core/models/user.model.ts
@@ -0,0 +1,19 @@
+export interface User {
+  id?: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password?: string;
+}
+
+export interface UserLogin {
+  email: string;
+  password: string;
+}
+
+export interface UserRegister {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
